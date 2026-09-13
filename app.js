ad5b537120bc10fa9b471c90fc9e0fa099a189a5
FDS_2023_1 <fds_2023_1@FDS-2023-1noMacBook-Pro.local>
2026-09-13 16:03:17 +0900

    Change default base color from pure blue to traffic light green

--- app.js
+++ app.js
@@ -209,7 +209,7 @@ async function startTrace() {
     .toUpperCase()
     .replace(/[^A-Z0-9\u30A0-\u30F6\u30FC\u30F3!?♡ \n]/g, '');
   const letterSize = parseInt(document.getElementById('size-select')?.value || '500');
-  const textColorHex = document.getElementById('color-picker')?.value || '#0000ff';
+  const textColorHex = document.getElementById('color-picker')?.value || '#00e56a';
   const checkbox = document.getElementById('random-color-checkbox');
   const isRandomColor = checkbox ? checkbox.checked : true;
 
